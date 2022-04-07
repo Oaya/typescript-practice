2c90db224af92e4cfcf923f7417e3724b5fc0101
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-04-07 12:07:19 -0700

    make the input form works

--- src/app.ts
+++ src/app.ts
@@ -57,10 +57,47 @@ class ProjectInput {
     this.attach();
   }
 
+  private gatherUserInput():
+    | [string, string, number]
+    | void {
+    const enteredTitle =
+      this.titleInputElement.value;
+    const enteredDescription =
+      this.descriptionInputElement.value;
+    const enteredPeople =
+      this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert("Invalid input value");
+      return;
+    } else {
+      return [
+        enteredTitle,
+        enteredDescription,
+        +enteredPeople,
+      ];
+    }
+  }
+
+  private clearInput() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
   @Autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInput();
+    }
   }
 
   private configure() {
