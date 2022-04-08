8a3575c07348debabbe68650f82b1480c2076f4e
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-04-07 21:05:38 -0700

    fix the duplication for the list

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
+//Project Type//
+enum ProjectStatus {
+  Active,
+  Finished,
+}
+
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
+}
+
 //Project State Management//
+type Listener = (items: Project[]) => void;
+
 class ProjectState {
-  private listeners: any[] = [];
-  private projects: any[] = [];
+  private listeners: Listener[] = [];
+  private projects: Project[] = [];
   private static instance: ProjectState;
 
   private constructor() {}
@@ -14,7 +32,7 @@ class ProjectState {
     return this.instance;
   }
 
-  addListener(listenerFn: Function) {
+  addListener(listenerFn: Listener) {
     this.listeners.push(listenerFn);
   }
 
@@ -23,12 +41,14 @@ class ProjectState {
     description: string,
     people: number
   ) {
-    const newProject = {
-      id: Math.random().toString(),
+    const newProject = new Project(
+      Math.random().toString(),
       title,
       description,
       people,
-    };
+      ProjectStatus.Active
+    );
+
     this.projects.push(newProject);
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice());
@@ -119,7 +139,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
-  assignedProjects: any[];
+  assignedProjects: Project[];
 
   constructor(
     private type: "active" | "finished"
@@ -140,9 +160,25 @@ class ProjectList {
     this.element =
       importedNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
+
     projectState.addListener(
-      (projects: any[]) => {
-        this.assignedProjects = projects;
+      (projects: Project[]) => {
+        const relevantProjects = projects.filter(
+          (prj) => {
+            if (this.type === "active") {
+              return (
+                prj.status ===
+                ProjectStatus.Active
+              );
+            } else {
+              return (
+                prj.status ===
+                ProjectStatus.Finished
+              );
+            }
+          }
+        );
+        this.assignedProjects = relevantProjects;
         this.renderProjects();
       }
     );
@@ -154,6 +190,7 @@ class ProjectList {
     const listEl = document.getElementById(
       `${this.type}-projects-list`
     ) as HTMLUListElement;
+    listEl.innerHTML = "";
     for (const prjItem of this.assignedProjects) {
       const listItem =
         document.createElement("li");
