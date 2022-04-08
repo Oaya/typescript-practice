82ebbed178362a181dcb799879e93071732956ef
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-04-07 23:04:27 -0700

    make dragover works

--- src/app.ts
+++ src/app.ts
@@ -230,7 +230,11 @@ class ProjectItem
 
   @Autobind
   dragStartHandler(event: DragEvent) {
-    console.log(event);
+    event.dataTransfer!.setData(
+      "text/plain",
+      this.project.id
+    );
+    event.dataTransfer!.effectAllowed = "move";
   }
 
   dragEndHandler(_: DragEvent) {
@@ -284,12 +288,21 @@ class ProjectList
   }
 
   @Autobind
-  dragOverHandler(_: DragEvent) {
-    const listEl =
-      this.element.querySelector("ul")!;
-    listEl.classList.add("droppable");
+  dragOverHandler(event: DragEvent) {
+    if (
+      event.dataTransfer &&
+      event.dataTransfer.types[0] === "text/plain"
+    ) {
+      event.preventDefault();
+      const listEl =
+        this.element.querySelector("ul")!;
+      listEl.classList.add("droppable");
+    }
   }
-  dropHandler(_: DragEvent) {}
+
+  dropHandler(event: DragEvent) {
+    console.log(event);
+  }
 
   @Autobind
   dragLeaveHandler(_: DragEvent) {
