6bce56a8fb96ef884a95884876e4c248c08165b6
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-04-07 22:43:37 -0700

    make the listitem dragable

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+//Drag & Drop Interfaces//
+interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void;
+  dropHandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
+}
+
 //Project Type//
 enum ProjectStatus {
   Active,
@@ -187,10 +199,13 @@ abstract class Component<
   abstract renderContent(): void;
 }
 //ProjectItem class//
-class ProjectItem extends Component<
-  HTMLUListElement,
-  HTMLLIElement
-> {
+class ProjectItem
+  extends Component<
+    HTMLUListElement,
+    HTMLLIElement
+  >
+  implements Draggable
+{
   private project: Project;
 
   get persons() {
@@ -213,7 +228,25 @@ class ProjectItem extends Component<
     this.renderContent();
   }
 
-  configure() {}
+  @Autobind
+  dragStartHandler(event: DragEvent) {
+    console.log(event);
+  }
+
+  dragEndHandler(_: DragEvent) {
+    console.log("DragEnd");
+  }
+
+  configure() {
+    this.element.addEventListener(
+      "dragstart",
+      this.dragStartHandler
+    );
+    this.element.addEventListener(
+      "dragend",
+      this.dragEndHandler
+    );
+  }
   renderContent() {
     this.element.querySelector(
       "h2"
